3c57571f6d921f298398aa3442e02313b4b5bca1
nickkreissler <nick.d.kreissler@gmail.com>
2018-01-18 00:03:48 -0600

    commit

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import urllib.request
 import ast
+import time
 
-def scrape():
-    x = urllib.request.urlopen("https://api.iextrading.com/1.0/ref-data/symbols")
+def scrapename():
+    x = urllib.request.urlopen("https://api.iextrading.com/1.0/ref-data/symbols?filter=symbol")
     x = x.read().decode('utf-8')
 
-    s = ""
-    for i in x:
-        if i == 't':
-            s += 'T'
-        elif i == 'f':
-            s += 'F'
-        else:
-            s += i
 
-    x = s.split('[{')
+
+    x = x.split('[{')
     x = x[1].split('}]')
     x = x[0]
     x = x.split('},{')
@@ -34,6 +28,36 @@ def scrape():
         y.append(z.copy())
 
     return y
+def getcurrent1():
+    import pandas as pd
+    import pandas.io.data as web  # Package and modules for importing data; this code may change depending on pandas version
+    import datetime
+
+    # We will look at stock prices over the past year, starting at January 1, 2016
+    start = datetime.datetime(2016, 1, 1)
+    end = datetime.date.today()
+
+    # Let's get Apple stock data; Apple's ticker symbol is AAPL
+    # First argument is the series we want, second is the source ("yahoo" for Yahoo! Finance), third is the start date, fourth is the end date
+    apple = web.DataReader("AAPL", "yahoo", start, end)
+
+    type(apple)
+def getcurrent():
+    print('running')
+    y = scrapename()
+    d = {}
+    z = time.clock()
+    for i in y:
+        try:
+            x = urllib.request.urlopen("https://api.iextrading.com/1.0/stock/{}/quote?filter=open".format(i['symbol']))
+
+            x = x.read().decode('utf-8')
 
 
-
+            d[i['symbol']] = ast.literal_eval(x)
+        except:
+            print('error')
+        print(len(d))
+    print(time.clock()-z)
+for i in scrapename():
+    print(i['symbol'])
